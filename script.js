34e3bc17a13d00a0ede0cb74a0473fa07a72ebb7
Oumaima Nejjari <oumanj1998@gmail.com>
2023-03-06 12:51:45 +0100

    Modify script.js file

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const menuBar = document.querySelector('.menu-bar');
 const mobileMenu = document.querySelector('.mobile-menu');
-menuBar.addEventListener('click', () => {
+
+const Bar = () => {
   menuBar.classList.toggle('active');
   mobileMenu.classList.toggle('active');
-});
+};
+
+menuBar.addEventListener('click', Bar);
+
+document.querySelectorAll('.menu-link').forEach((element) => element.addEventListener('click', () => {
+  menuBar.classList.remove('active');
+  mobileMenu.classList.remove('active');
+}));
