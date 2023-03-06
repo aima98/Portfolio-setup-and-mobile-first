d7e3202fa13c8f6b45c8e3338d9cdbd1b3004d22
Oumaima Nejjari <oumanj1998@gmail.com>
2023-03-06 12:20:21 +0100

    Configuring linters

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-const menuBar= document.querySelector(".menu-bar");
-const mobileMenu= document.querySelector(".mobile-menu");
-
-menuBar.addEventListener("click", () => {
-    menuBar.classList.toggle("active");
-    mobileMenu.classList.toggle("active");
-    closeIcon.classList.toggle("active");
+const menuBar = document.querySelector('.menu-bar');
+const mobileMenu = document.querySelector('.mobile-menu');
+menuBar.addEventListener('click', () => {
+  menuBar.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
 });
