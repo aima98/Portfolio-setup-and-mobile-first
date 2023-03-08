a9357b8b75525a115f0948da03c00a6ecb4cee84
Oumaima Nejjari <oumanj1998@gmail.com>
2023-03-08 01:50:13 +0100

    Modify index.js file

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const menuBar = document.querySelector('.menu-bar');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+const Bar = () => {
+  menuBar.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+};
+
+menuBar.addEventListener('click', Bar);
+
+document.querySelectorAll('.menu-link').forEach((element) => element.addEventListener('click', () => {
+  menuBar.classList.remove('active');
+  mobileMenu.classList.remove('active');
+}));
+
 const data = [
   {
     id: 'Card1',
@@ -419,34 +434,3 @@ for (let j = 1; j <= DATA; j++) {
     });
   });
 }
-
-const hamburger = document.querySelector('.menu-button');
-const menuClose = document.querySelector('.close-icon');
-const menuOverlay = document.getElementById('menu-overlay');
-const entireOver = document.querySelector('.mobile-menu');
-
-const displayMenuList = () => {
-  hamburger.style.display = 'none';
-  menuOverlay.style.display = 'block';
-  entireOver.style.display = 'block';
-  menuClose.style.display = 'block';
-};
-
-const closeMenuList = () => {
-  menuOverlay.style.display = 'none';
-  hamburger.style.display = 'block';
-  entireOver.style.display = 'none';
-  menuClose.style.display = 'none';
-};
-
-hamburger.addEventListener('click', displayMenuList);
-menuClose.addEventListener('click', closeMenuList);
-
-document.querySelectorAll('.menu-item-list').forEach((n) => {
-  n.addEventListener('click', () => {
-    menuOverlay.style.display = 'none';
-    hamburger.style.display = 'block';
-    menuClose.style.display = 'none';
-    entireOver.style.display = 'none';
-  });
-});
